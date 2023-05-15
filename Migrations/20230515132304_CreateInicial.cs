fe362899486a266230901d719576fc2846f2dafc
HenriqueMafort <hmafort@gmail.com>
2023-05-15 11:09:14 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20230515132304_CreateInicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TerceiroCRUD.Migrations
+{
+    public partial class CreateInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    FuncionarioId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    NomeCompleto = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    CodFuncionario = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Funcao = table.Column<string>(type: "varchar(100)", nullable: true),
+                    LocalDeTrabalho = table.Column<string>(type: "varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionarios", x => x.FuncionarioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Funcionarios");
+        }
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerceiroCRUD.Models
+{
+    public class Funcionario
+    {
+        [Key]
+        public int FuncionarioId { get; set; }
+        
+        [Required(ErrorMessage ="Este Campo é de Preenchimento Obrigatório")]
+        [Column(TypeName ="nvarchar(250)")]
+        [DisplayName("Nome")]
+        public string NomeCompleto { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Cod. Funcionário")]
+        public int CodFuncionario { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Função")]
+        public string Funcao { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Local De Trabalho")]
+        public string LocalDeTrabalho { get; set; }
+    }
+}
